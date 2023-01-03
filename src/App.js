62e3d61d58522255d09e13a0d656a7df2848d9a8
Hakan AKSOY <aksoyhakan1990gmail.com>
2023-01-03 16:26:38 +0300

    Tüm görevler yapılmıştır.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@
 */
 
 // State hook u import edin
-import React from 'react';
+import React, { useState } from "react";
+import BeğenBölümü from "./bileşenler/Gönderiler/BeğenBölümü";
+import Gönderi from "./bileşenler/Gönderiler/Gönderi";
+import GönderiBaşlığı from "./bileşenler/Gönderiler/GönderiBaşlığı";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler";
+import "./bileşenler/Gönderiler/Gönderiler.css";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
+import "./bileşenler/AramaÇubuğu/AramaÇubuğu.css";
 
+import sahteVeri from "./sahte-veri";
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+import "./App.css";
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
-    /*
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [aramaKriteri, setAramaKriteri] = useState("");
+  const gonderiyiBegen = (gonderiID) => {
+    let yeniGönderi = [];
+    yeniGönderi = gonderiler.map((item) => {
+      if (item.id === gonderiID) item.likes++;
+      return item;
+    });
+    setGonderiler(yeniGönderi);
+  };
+  /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
@@ -28,12 +44,19 @@ const App = () => {
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
-  };
 
   return (
-    <div className='App'>
-      {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
-      {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
+    <div className="App">
+      {
+        <AramaÇubuğu />
+        /* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */
+      }
+      {
+        <Gönderiler
+          gonderiyiBegen={gonderiyiBegen}
+          gonderiler={gonderiler}
+        /> /* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */
+      }
     </div>
   );
 };
